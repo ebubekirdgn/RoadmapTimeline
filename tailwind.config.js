0b53dd4915e1f05cb9cc7ff64ee428901b4bc05d
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-06-22 15:53:50 +0300

    update tailwind darkmode false

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'false', // <-- burayı ekle
+  darkMode: false, // <-- burayı ekle
   theme: {
     extend: {},
   },
